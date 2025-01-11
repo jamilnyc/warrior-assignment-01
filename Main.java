9d481abc740c88150f911910fbc00a080fb832f4
Murshed J. Ahmed <mahmed@marvel.com>
2025-01-11 07:49:30 -0500

    Add sample Main.java

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        // Read the input file and start the simulation
+        System.out.println("Welcome to the Warrior Fight Simulation!");
+    }
+}
